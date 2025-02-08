461c0c2be8f6c64587d18a335223d5eb1d715c24
BxDxnxxl <danielcaton44@gmail.com>
2025-02-08 18:09:20 +0100

    Logica de services, repository y controller

---
+++ Service/IUsuarioService.cs
@@ -0,0 +1,15 @@
+using Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Videojuegos.Service
+{
+    public interface IUsuarioService
+    {
+        Task<List<Usuario>> GetAllAsync();
+        Task<Usuario?> GetByIdAsync(int id);
+        Task AddAsync(Usuario usuario);
+        Task UpdateAsync(Usuario usuario);
+        Task DeleteAsync(int id);
+    }
+}
